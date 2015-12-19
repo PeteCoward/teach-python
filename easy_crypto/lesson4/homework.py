5432fa7a58072e232341e9f412848520730384d5
PeteCoward <peter.coward@gmail.com>
2015-12-19 10:25:10 +0900

    further lesson4 homework

--- easy_crypto/lesson4/homework.py
+++ easy_crypto/lesson4/homework.py
@@ -3,4 +3,10 @@
 
 
 def count_letter_a(input_string):
+    ''' returns the number of 'a's in the input string '''
+    raise NotImplementedError
+
+
+def count_letter(input_string, letter_to_count):
+    ''' returns the number of 'a's in the input string '''
     raise NotImplementedError
--- easy_crypto/lesson4/tests.py
+++ easy_crypto/lesson4/tests.py
@@ -1,7 +1,12 @@
-from .homework import count_letter_a
+from .homework import count_letter_a, count_letter
 
 
 def test_count_letter_a():
     assert count_letter_a("hello") == 0
     assert count_letter_a("bondia") == 1
     assert count_letter_a("agora") == 2
+    assert count_letter_a("aaaaaaaaaa") == 10
+
+
+def test_count_letter():
+    assert count_letter("hello", 'e') == 1
